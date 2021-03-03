6bf350b7f3a7befe9dc9b5d912fb76cfcd0d9199
Dawid <baluszynskidawid@gmail.com>
2021-03-03 23:39:21 +0100

    feat: add player turns for propper marking
    Choosed approach for making such logic in Players factory to being able to register actions on all Players instances

--- scripts/main.js
+++ scripts/main.js
@@ -1,85 +1,112 @@
- const app = (() => {
+const app = (() => {
 
-     const gameBoard = (() => {
-         
+    const gameBoard = (() => {
+
         // Privates
-        const gameBoard = [
-             '', '', '',
-             '', '', '',
-             '', '', ''
-         ];
+        const _gameBoard = [
+            '', '', '',
+            '', '', '',
+            '', '', ''
+        ];
 
-         const board = document.querySelector('.gameBoard');
+        const _addHighlight = (e) => {
+            if (e.target.matches('.gameBoard__cell')) {
+                e.target.classList.add('highlight');
+            }
+        };
 
-         const isCellEmpty = (index) => {
-            return gameBoard[index].length < 1;
-         }
+        const _removeHightlight = (e) => {
+            if (e.target.matches('.gameBoard__cell')) {
+                e.target.classList.remove('highlight');
+            }
+        };
 
-         const _addHighlight = (e) => {
-             if (e.target.matches('.gameBoard__cell')) {
-                 e.target.classList.add('highlight');
-             }
-         }
-
-         const _removeHightlight = (e) => {
-             if (e.target.matches('.gameBoard__cell')) {
-                 e.target.classList.remove('highlight');
-             }
-
-         }
-
-         board.addEventListener('mouseover', _addHighlight);
-         board.addEventListener('mouseout', _removeHightlight);
         // Public
-         const renderGameBoard = () => {
-            board.innerHTML = gameBoard.map((cell, index) => {
-            return(
-                `
+        const getGameBoard = () => { return _gameBoard };
+
+        const board = document.querySelector('.gameBoard');
+
+        const isCellEmpty = (index) => {
+            return _gameBoard[index].length < 1;
+        };
+
+        const renderGameBoard = () => {
+            board.innerHTML = _gameBoard.map((cell, index) => {
+                return (
+                    `
                 <div class="gameBoard__cell" data-index=${index}>${cell}</div>
                 `
-            )}).join('');
-         }
+                )
+            }).join('');
+        };
 
-         // Returning
-         return { gameBoard, board, renderGameBoard, isCellEmpty }
+        // Events
+        board.addEventListener('mouseover', _addHighlight);
+        board.addEventListener('mouseout', _removeHightlight);
 
-     })();
 
-     const Players = (name, mark) => {
+        // Returning
+        return { getGameBoard, board, renderGameBoard, isCellEmpty }
+
+    })();
+
+
+    const Players = (name, mark) => {
         const _name = name;
         const _mark = mark;
+        let _gameWon = false;
+
+        // If you choosed 'X' mark, first turn is yours
+        let _yourTurn = mark === 'X';
+
+        const _markCellBoard = (e) => {
+            e.preventDefault();
+            if (_yourTurn && !gameController.isGameOver) {
+                const cellIndex = e.target.dataset.index;
+                if (gameBoard.isCellEmpty(cellIndex)) {
+                    const board = gameBoard.getGameBoard();
+                    board[cellIndex] = mark;
+                    gameBoard.renderGameBoard();
+                    gameController.changeTurn();
+                }
+            }
+        }
+        
+        const changeTurn = () => {
+            _yourTurn = !_yourTurn;
+        };
 
         const getName = () => _name;
+        const getMark = () => _mark;
 
-        function markCellBoard(e) {
-            e.preventDefault();
-            const cellIndex = e.target.dataset.index;
-            if(gameBoard.isCellEmpty(cellIndex))
-            {
-                gameBoard.gameBoard[cellIndex] = mark;
-                gameBoard.renderGameBoard();
-            }
-            
+        gameBoard.board.addEventListener('mousedown', _markCellBoard);
+
+        return { getName, getMark, changeTurn };
+    }
+
+
+    const gameController = (() => {
+
+        const changeTurn = () => {
+            playerOne.changeTurn();
+            playerTwo.changeTurn();
         }
 
-        gameBoard.board.addEventListener('mousedown', markCellBoard);
-        return { getName };
-     }
+        let isGameOver = false;
+        return { isGameOver, changeTurn }
+    })();
 
-     const gameController = (() => {
-         
-     })();
+    const options = (() => {
 
-     const options = (() => {
+    })();
 
-     })();
 
-     const playerOne = Players('P1', 'X');
-    //  const playerTwo = Players('P2', 'O');
+    const playerOne = Players('P1', 'X');
+    const playerTwo = Players('P2', 'O');
 
-     gameBoard.renderGameBoard();
+    gameBoard.renderGameBoard();
 
- })
+})
 
 app();
 
